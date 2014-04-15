a7750e6d8f6f6be579290fe3a2ef4cb5a73533e8
giantsfan <thegurugamers@gmail.com>
2014-04-14 21:39:43 -0400

    First Commit

---
+++ src/me/projectx/Settlements/Main.java
@@ -0,0 +1,19 @@
+package me.projectx.Settlements;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	private static Main plugin;
+	
+	public void onEnable(){
+		plugin = this;
+	}
+	
+	public void onDisable(){
+		plugin = null;
+	}
+	
+	public static Main getInstance(){
+		return plugin;
+	}
+}
