0ee98465cd96ae9ce9ab469f41f65e1c3fddeb20
giantsfan <thegurugamers@gmail.com>
2014-04-18 23:39:32 -0400

    Entering/Leaving claimed land messages now run on separate thread

--- src/me/projectx/Settlements/Events/PlayerMove.java
+++ src/me/projectx/Settlements/Events/PlayerMove.java
@@ -9,26 +9,30 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-public class PlayerMove implements Listener{
+public class PlayerMove extends Thread implements Listener{
 	
 	@EventHandler
-	public void onMove(PlayerMoveEvent e){
-		if (e.getFrom().getChunk() != e.getTo().getChunk()){
-			ClaimedChunk c = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ());
-			ClaimedChunk d = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ());
-			if(c==null&&d==null){
-			}else if(c!=null&&d==null){ //Leaving b to non claimed
-				e.getPlayer().sendMessage(ChatColor.GREEN + "~Wilderness");
-			}else if(d!=null&&c==null){ //Entering a from unclaimed
-				Settlement a = d.getSettlement();
-				e.getPlayer().sendMessage(a.getName() + " ~ " + a.getDescription());
-			}else if(c!=null&&d!=null){ //Entering one claim to another claim
-				Settlement a = d.getSettlement();
-				Settlement b = c.getSettlement();
-				if(!a.equals(b)){
-					e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + a.getName() + " entering " + b.getName());
-				}      
+	public void onMove(final PlayerMoveEvent e){
+		new Thread() {
+			@Override
+			public void run() {
+				if (e.getFrom().getChunk() != e.getTo().getChunk()){
+					ClaimedChunk c = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ());
+					ClaimedChunk d = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ());
+					if(c==null&&d==null){
+					}else if(c!=null&&d==null){ //Leaving b to non claimed
+						e.getPlayer().sendMessage(ChatColor.GREEN + "~Wilderness");
+					}else if(d!=null&&c==null){ //Entering a from unclaimed
+						Settlement a = d.getSettlement();
+						e.getPlayer().sendMessage(a.getName() + " ~ " + a.getDescription());
+					}else if(c!=null&&d!=null){ //Entering one claim to another claim
+						Settlement a = d.getSettlement();
+						Settlement b = c.getSettlement();
+						if(!a.equals(b))
+							e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + a.getName() + " entering " + b.getName());    
+					}
+				}	     
 			}
-		}	     
+		}.start();
 	}
 }
