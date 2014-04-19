85be0ef3c98bcd038689406cec575b15ceec8024
giantsfan <thegurugamers@gmail.com>
2014-04-18 20:32:38 -0400

    Attempt to fix entering claimed land messages

--- src/me/projectx/Settlements/Events/PlayerMove.java
+++ src/me/projectx/Settlements/Events/PlayerMove.java
@@ -2,6 +2,7 @@ package me.projectx.Settlements.Events;
 
 import java.util.HashMap;
 
+import me.projectx.Settlements.API.Settlement;
 import me.projectx.Settlements.API.SettlementManager;
 import me.projectx.Settlements.Land.ChunkManager;
 import me.projectx.Settlements.Land.ClaimedChunk;
@@ -13,15 +14,34 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMove implements Listener{
 
-	@EventHandler
+	/*@EventHandler
 	public void onMove(PlayerMoveEvent e){
 		if (ChunkManager.getInstance().isInChunk(e.getPlayer())){
 			if (e.getFrom().getChunk() != e.getTo().getChunk()){
-				if (ChunkManager.getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ()).getSettlement() != 
-						ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement()){
-					ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());	
+				if (ChunkManager.getInstance().isClaimed(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ())){
+					/*if (!ChunkManager.getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ()).getSettlement().equals(
+	                                      ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement())){
+						ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());      
+					//}
 				}
 			}
 		}	
-	}
+	}*/
+	
+	@EventHandler
+    public void onMove(PlayerMoveEvent e){
+		if (ChunkManager.getInstance().isInChunk(e.getPlayer())){
+			if (e.getFrom().getChunk() != e.getTo().getChunk()){
+				Settlement a = ChunkManager.getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ()).getSettlement();
+				//Settlement b = ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement();
+				if (a != null && ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement() !=null && a!=ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement()){
+					ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());      
+				}else if(ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement()!=null && a==null){
+					ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());
+				}else if(a!=null&&ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement()==null){
+						e.getPlayer().sendMessage("Leaving claimed territory");
+				}
+			}
+		}      
+    }
 }
