e546d7f2f36b61cd4a94bacd7a3471ab577d0413
mattrick16 <matthewhighrollers@gmail.com>
2014-04-18 18:05:55 -0700

    Use Ash's methods

--- src/me/projectx/Settlements/Events/PlayerMove.java
+++ src/me/projectx/Settlements/Events/PlayerMove.java
@@ -2,6 +2,7 @@ package me.projectx.Settlements.Events;
 
 import me.projectx.Settlements.API.Settlement;
 import me.projectx.Settlements.Land.ChunkManager;
+import me.projectx.Settlements.Land.ClaimedChunk;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,14 +28,24 @@ public class PlayerMove implements Listener{
 	public void onMove(PlayerMoveEvent e){
 		if (ChunkManager.getInstance().isInChunk(e.getPlayer())){
 			if (e.getFrom().getChunk() != e.getTo().getChunk()){
-				Settlement a = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ()).getSettlement();
-				//Settlement b = ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement();
-				if (a != null && ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement() !=null && a!=ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement()){
-					ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());      
-				}else if(ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement()!=null && a==null){
-					ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());
-				}else if(a!=null&&ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement()==null){
-					e.getPlayer().sendMessage("Leaving claimed territory");
+				ClaimedChunk c = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ());
+				ClaimedChunk d = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ());
+				if(c==null&&d==null){
+					//both arent claimed
+				}else if(c!=null&&d==null){
+					Settlement b = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement();
+					//Entering b from non claimed
+				}else if(d!=null&&c==null){
+					Settlement a = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ()).getSettlement();
+					//Entering nonclaimed from a
+				}else if(c!=null&&d!=null){
+					Settlement a = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ()).getSettlement();
+					Settlement b = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement();
+
+					if(a!=b){
+						//Entering b
+						ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());
+					}      
 				}
 			}
 		}      
