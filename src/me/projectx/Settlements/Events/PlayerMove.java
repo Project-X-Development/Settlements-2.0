8b92ffb6046e93499edc1a29707d488c77e376bc
giantsfan <thegurugamers@gmail.com>
2014-04-18 22:44:03 -0400

    Messages correctly sent to players when entering/leaving claimed land

--- src/me/projectx/Settlements/Events/PlayerMove.java
+++ src/me/projectx/Settlements/Events/PlayerMove.java
@@ -10,54 +10,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMove implements Listener{
-
-	/*@EventHandler
-	public void onMove(PlayerMoveEvent e){
-		if (ChunkManager.getInstance().isInChunk(e.getPlayer())){
-			if (e.getFrom().getChunk() != e.getTo().getChunk()){
-				if (ChunkManager.getInstance().isClaimed(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ())){
-					/*if (!ChunkManager.getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ()).getSettlement().equals(
-	                                      ChunkManager.getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ()).getSettlement())){
-						ChunkManager.getInstance().sendInChunkMsg(e.getPlayer());      
-					//}
-				}
-			}
-		}	
-	}*/
-
+	
 	@EventHandler
 	public void onMove(PlayerMoveEvent e){
-		if (ChunkManager.getInstance().isInChunk(e.getPlayer())){
-			if (e.getFrom().getChunk() != e.getTo().getChunk()){
-				ClaimedChunk c = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ());
-				ClaimedChunk d = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ());
-				if(c==null&&d==null){
-					e.getPlayer().sendMessage("Null"); //never sent
-				}else if(c!=null&&d==null){
-					Settlement b = c.getSettlement();
-					//Leaving b to non claimed
-					e.getPlayer().sendMessage(ChatColor.GREEN + "~Wilderness"); //sent when leaving claimed land
-				}else if(d!=null&&c==null){
-					Settlement a = d.getSettlement();
-					//Entering a from unclaimed
-					e.getPlayer().sendMessage("Test~Wilderness"); //never sent
-				}else if(c!=null&&d!=null){
-					Settlement a = d.getSettlement();
-					Settlement b = c.getSettlement();
-					if(!a.equals(b)){
-						//Entering b
-						ChunkManager.getInstance().sendInChunkMsg(e.getPlayer()); //never sent
-					}      
-				}
-				
-				if (c == null){
-					e.getPlayer().sendMessage("c = null");
-				}
-				
-				if (d == null){
-					e.getPlayer().sendMessage("d = null");
-				}
+		if (e.getFrom().getChunk() != e.getTo().getChunk()){
+			ClaimedChunk c = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ());
+			ClaimedChunk d = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ());
+			if(c==null&&d==null){
+			}else if(c!=null&&d==null){ //Leaving b to non claimed
+				e.getPlayer().sendMessage(ChatColor.GREEN + "~Wilderness");
+			}else if(d!=null&&c==null){ //Entering a from unclaimed
+				Settlement a = d.getSettlement();
+				e.getPlayer().sendMessage(a.getName() + " ~ " + a.getDescription());
+			}else if(c!=null&&d!=null){ //Entering one claim to another claim
+				Settlement a = d.getSettlement();
+				Settlement b = c.getSettlement();
+				if(!a.equals(b)){
+					e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + a.getName() + " entering " + b.getName());
+				}      
 			}
-		}      
+		}	     
 	}
 }
