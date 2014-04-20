9b5824e1f9fc3222faad2da6f4fb34b9379a4d27
mattrick16 <matthewhighrollers@gmail.com>
2014-04-19 21:18:32 -0700

    This will hopefully work

--- src/me/projectx/Settlements/Events/PlayerJoin.java
+++ src/me/projectx/Settlements/Events/PlayerJoin.java
@@ -1,5 +1,7 @@
 package me.projectx.Settlements.Events;
 
+import me.projectx.Settlements.Managers.SettlementManager;
+import me.projectx.Settlements.Models.Settlement;
 import me.projectx.Settlements.Utils.PlayerCache;
 
 import org.bukkit.event.EventHandler;
@@ -7,9 +9,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoin implements Listener {
-	
+
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e){
 		PlayerCache.getCache().put(e.getPlayer().getName(), e.getPlayer().getUniqueId());
+		if (SettlementManager.getManager().isCitizenOfSettlement(e.getPlayer().getName())){
+			Settlement set = SettlementManager.getManager().getPlayerSettlement(e.getPlayer().getName());
+			set.getTeam().addPlayer(e.getPlayer());
+		}
 	}
 }
