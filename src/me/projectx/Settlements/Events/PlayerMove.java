04dd578654f723f32813e6d39997098ce9b79179
mattrick <matthewhighrollers@gmail.com>
2014-05-11 12:36:44 -0700

    Possibly fix NPE

--- src/me/projectx/Settlements/Events/PlayerMove.java
+++ src/me/projectx/Settlements/Events/PlayerMove.java
@@ -17,36 +17,36 @@ public class PlayerMove implements Listener{
 
 	@EventHandler
 	public void onMove(final PlayerMoveEvent e){
-			if (e.getFrom().getChunk() != e.getTo().getChunk()){
-				ClaimedChunk c = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ());
-				ClaimedChunk d = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ());
-					if(c==null&&d==null){
-					}else if(c!=null&&d==null){ //Leaving b to non claimed
-						e.getPlayer().sendMessage(ChatColor.GREEN + "~Wilderness");
-					}else if(d!=null&&c==null){ //Entering a from unclaimed
-						Settlement a = d.getSettlement();
-						if (d.getType() == ClaimType.NORMAL) {
-							e.getPlayer().sendMessage(ChatColor.AQUA + a.getName() + ChatColor.RED + " ~ " + ChatColor.GRAY + a.getDescription());
-						} else if (d.getType() == ClaimType.SAFEZONE) {
-							e.getPlayer().sendMessage(ChatColor.GOLD + "SafeZone ~ Safe from PvP and Monsters");
-						}
-					}else if(c!=null&&d!=null){ //Entering one claim to another claim
-						Settlement a = c.getSettlement();
-						Settlement b = d.getSettlement();
-						
-						if (c.getType() == ClaimType.SAFEZONE && d.getType() != ClaimType.SAFEZONE){
-							e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + ChatColor.GOLD + "SafeZone" + ChatColor.GREEN + ", entering " + b.getName());
-						}
-			
-						if(!a.equals(b)){ //NPE
-							if (d.getType() == ClaimType.NORMAL) {
-								e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + a.getName() + " entering " + b.getName());
-							} else if (d.getType() == ClaimType.SAFEZONE) {
-								e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + a.getName() + ", entering " + ChatColor.GOLD + "SafeZone");
-							}
-						}				
-					} //add for safezone & battleground
-				}	     
+		if (e.getFrom().getChunk() != e.getTo().getChunk()){
+			ClaimedChunk c = ChunkManager.getInstance().getChunk(e.getFrom().getChunk().getX(), e.getFrom().getChunk().getZ());
+			ClaimedChunk d = ChunkManager.getInstance().getChunk(e.getTo().getChunk().getX(), e.getTo().getChunk().getZ());
+			if(c==null&&d==null){
+			}else if(c!=null&&d==null){ //Leaving b to non claimed
+				e.getPlayer().sendMessage(ChatColor.GREEN + "~Wilderness");
+			}else if(d!=null&&c==null){ //Entering a from unclaimed
+				Settlement a = d.getSettlement();
+				if (d.getType() == ClaimType.NORMAL) {
+					e.getPlayer().sendMessage(ChatColor.AQUA + a.getName() + ChatColor.RED + " ~ " + ChatColor.GRAY + a.getDescription());
+				} else if (d.getType() == ClaimType.SAFEZONE) {
+					e.getPlayer().sendMessage(ChatColor.GOLD + "SafeZone ~ Safe from PvP and Monsters");
+				}
+			}else if(c!=null&&d!=null){ //Entering one claim to another claim
+				Settlement a = c.getSettlement();
+				Settlement b = d.getSettlement();
+
+				if (c.getType() == ClaimType.SAFEZONE && d.getType() != ClaimType.SAFEZONE){
+					e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + ChatColor.GOLD + "SafeZone" + ChatColor.GREEN + ", entering " + b.getName());
+				}
+
+				else if(!a.equals(b)){ //NPE
+					if (d.getType() == ClaimType.NORMAL) {
+						e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + a.getName() + " entering " + b.getName());
+					} else if (d.getType() == ClaimType.SAFEZONE) {
+						e.getPlayer().sendMessage(ChatColor.GREEN + "Leaving " + a.getName() + ", entering " + ChatColor.GOLD + "SafeZone");
+					}
+				}				
+			} //add for safezone & battleground
+		}	     
 
 
 		if (ChunkManager.getInstance().isAutoClaiming(e.getPlayer())){
