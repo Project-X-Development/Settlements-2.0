d32a89b04951d8df770206ba6f8f73e2922fd339
Ashy <ashle.thew@gmail.com>
2014-06-18 19:23:06 +1200

    Fixed map

--- src/me/projectx/Settlements/Utils/RenderMap.java
+++ src/me/projectx/Settlements/Utils/RenderMap.java
@@ -1,6 +1,7 @@
 package me.projectx.Settlements.Utils;
 
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +19,7 @@ import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
 
 public class RenderMap extends MapRenderer {
-	
+
 	private List<String> players = new ArrayList<String>();
 
 	@SuppressWarnings("deprecation")
@@ -39,41 +40,64 @@ public class RenderMap extends MapRenderer {
 			for(int z = 1; z<=25; z++){
 				int column = 1+(4*x)-3+(x-1);
 				int row = 1+(4*z)-3+(z-1);
-				int chunkx = p.getLocation().getChunk().getX()-(x-13);
-				int chunkz = p.getLocation().getChunk().getZ()-(z-13);
-				for(int a = 0; a<4; a++){
-					for(int b = 0; b<4; b++){
-						ClaimedChunk c = cm.getChunk(chunkx, chunkz);
-						if(cm.isClaimed(chunkx, chunkz)&&c.getSettlement()==set){//If chunk x,z is claimed by own settlement
-							canvas.setPixel(column + a, row + b, MapPalette.LIGHT_GREEN);
-						}else if(cm.isClaimed(chunkx, chunkz)&&c.getSettlement()!=set){//If chunk x,z is claimed by other settlement
-							canvas.setPixel(column + a, row + b, MapPalette.RED);cm.getChunk(chunkx, chunkx);
+				int chunkx = p.getLocation().getChunk().getX()+(x-13);
+				int chunkz = p.getLocation().getChunk().getZ()+(z-13);
+				ClaimedChunk c = cm.getChunk(chunkx, chunkz);
+
+				if(cm.isClaimed(chunkx, chunkz)){
+					if(c.getType() == ClaimType.SAFEZONE){
+						for(int a = 0; a<4; a++){
+							for(int b = 0; b<4; b++){
+								canvas.setPixel(column + a, row + b, MapPalette.BLUE);
+							}
+						}
+					}else{
+						if(c.getSettlement()==set){
+							for(int a = 0; a<4; a++){
+								for(int b = 0; b<4; b++){
+									canvas.setPixel(column + a, row + b, MapPalette.LIGHT_GREEN);
+								}
+							}
 						}else{
+							for(int a = 0; a<4; a++){
+								for(int b = 0; b<4; b++){
+									canvas.setPixel(column + a, row + b, MapPalette.RED);
+								}
+							}
+						}
+					}
+				}else{
+					for(int a = 0; a<4; a++){
+						for(int b = 0; b<4; b++){
 							canvas.setPixel(column + a, row + b, MapPalette.LIGHT_GRAY);
 						}
-						if(x==13&&z==13){
+					}
+				}
+				if(x==13&&z==13){
+					for(int a = 0; a<4; a++){
+						for(int b = 0; b<4; b++){
 							if((a==1&&b==1)||(a==1&&b==2)||(a==2&&b==1)||(a==2&&b==2)){
 								canvas.setPixel(column + a, row + b, MapPalette.PALE_BLUE);
 							}
-						}
-					}	
+						}	
+					}
 				}
 			}
 		}
 		p.sendMap(view);
 	}
-	
+
 	public void add(Player pl){
 		this.players.add(pl.getUniqueId().toString());
 	}
-	
+
 	public void remove(Player pl){
 		String uuid = pl.getUniqueId().toString();
 		if(this.players.contains(uuid)){
 			this.players.remove(uuid);
 		}
 	}
-	
+
 	public boolean contains(Player pl){
 		String uuid = pl.getUniqueId().toString();
 		if(this.players.contains(uuid)){
