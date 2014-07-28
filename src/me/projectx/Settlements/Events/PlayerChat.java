1f35c266ffcf6dd150c0dffd2eac8a2a32fe51a4
giantsfan <thegurugamers@gmail.com>
2014-07-28 18:38:01 -0400

    Only need to cancel the event once

--- src/me/projectx/Settlements/Events/PlayerChat.java
+++ src/me/projectx/Settlements/Events/PlayerChat.java
@@ -18,18 +18,16 @@ public class PlayerChat implements Listener {
 	public void onChat(AsyncPlayerChatEvent e){
 		Settlement s = SettlementManager.getManager().getPlayerSettlement(e.getPlayer().getUniqueId());
 		if (s != null){
+			e.setCancelled(true);
 			if (PlayerManager.getInstance().isAllianceChatting(e.getPlayer())){
-				e.setCancelled(true);
 				s.sendAllianceMessage(ChatColor.LIGHT_PURPLE + "[" + e.getPlayer().getDisplayName() + 
 						ChatColor.LIGHT_PURPLE + "] " + e.getFormat());
 				System.out.println("[Settlements] Alliance Chat: " + e.getPlayer().getName() + " - " + e.getMessage());
 			}else if (PlayerManager.getInstance().isSettlementChatting(e.getPlayer())){
-				e.setCancelled(true);
 				s.sendSettlementMessage(ChatColor.GREEN + "[" + e.getPlayer().getDisplayName() + 
 						ChatColor.GREEN + "] " + e.getMessage());
 				System.out.println("[Settlements] Settlement Chat: " + e.getPlayer().getName() + " - " + e.getMessage());
 			}else{
-				e.setCancelled(true);
 				for (Player p : Bukkit.getOnlinePlayers())
 					ChatManager.getManager().formatMessage(p, e.getPlayer().getName(), e.getMessage());
 				System.out.println("[" + s.getName() + "] " + e.getPlayer().getName() + ": " + e.getMessage());
