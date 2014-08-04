f04c5a8c27404e757621894196a6586246849a36
Ashy <ashle.thew@gmail.com>
2014-08-04 17:44:36 +1200

    Cleanup after editing maps

--- src/me/projectx/settlements/events/PlayerQuit.java
+++ src/me/projectx/settlements/events/PlayerQuit.java
@@ -1,9 +1,12 @@
 package me.projectx.settlements.events;
 
+import java.io.File;
+
 import me.projectx.settlements.managers.ChunkManager;
 import me.projectx.settlements.managers.MapManager;
 import me.projectx.settlements.managers.PlayerManager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
@@ -14,6 +17,8 @@ public class PlayerQuit implements Listener{
 	@EventHandler
 	public void onQuit(PlayerQuitEvent e){
 		MapManager.getInstance().remove(e.getPlayer());
+		File f = new File(Bukkit.getWorldContainer(), Bukkit.getWorlds().get(0).getName() + "/data/map_" + MapManager.getInstance().getPlayerMapID(e.getPlayer()));
+		f.delete();
 		PlayerManager.getInstance().removePlayer(e.getPlayer());
 		ChunkManager.getManager().autoClaim.remove(e.getPlayer().getName());
 	}
@@ -21,6 +26,8 @@ public class PlayerQuit implements Listener{
 	@EventHandler
 	public void onQuit(PlayerKickEvent e){
 		MapManager.getInstance().remove(e.getPlayer());
+		File f = new File(Bukkit.getWorldContainer(), Bukkit.getWorlds().get(0).getName() + "/data/map_" + MapManager.getInstance().getPlayerMapID(e.getPlayer()));
+		f.delete();
 		PlayerManager.getInstance().removePlayer(e.getPlayer());
 		ChunkManager.getManager().autoClaim.remove(e.getPlayer().getName());
 	}
