eb7f96a98357056eb7448d7263b13b775970ddb9
mattrick <matthewhighrollers@gmail.com>
2014-11-23 16:12:25 -0800

    Change settlement deletion - Fixes #1

--- src/me/projectx/settlements/utils/DatabaseUtils.java
+++ src/me/projectx/settlements/utils/DatabaseUtils.java
@@ -12,11 +12,11 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 /*
  * TODO Add prepared statements to prevent SQL injection
- * 
+ *
  * I honestly have no idea how to do this....
- * 
+ *
  * Someone halp plz
- * 
+ *
  */
 
 public class DatabaseUtils {
@@ -43,7 +43,7 @@ public class DatabaseUtils {
 
 	public static void setupMySQL() throws SQLException {
 		queryOut("CREATE TABLE IF NOT EXISTS settlements(id BIGINT, name varchar(255), "
-				+ "leader varchar(255), description varchar(255), balance BIGINT);");
+				+ "leader varchar(255), description varchar(255), balance BIGINT, deleted tinyint(1));");
 		queryOut("CREATE TABLE IF NOT EXISTS citizens(uuid varchar(255), settlement varchar(255), rank varchar(255));");
 		queryOut("CREATE TABLE IF NOT EXISTS sethomes(id BIGINT, world VARCHAR(255), x BIGINT, y BIGINT, z BIGINT, yaw BIGINT, pitch BIGINT);");
 		queryOut("CREATE TABLE IF NOT EXISTS chunks(x BIGINT, z BIGINT, player VARCHAR(255), settlement BIGINT, world VARCHAR(255), type VARCHAR(255));");
@@ -76,7 +76,7 @@ public class DatabaseUtils {
 	}
 
 	public static void queryOut(final String query) throws SQLException {
-		 new Thread() {
+		new Thread() {
 			@Override
 			public void run() {
 				Statement statement;
